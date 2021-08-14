425dbe55679486f0d500f0fb072d9e9af80fedad
Loliticos <familiadojorge@outlook.com>
2021-08-14 16:42:06 -0300

    fix() create request logic

--- src/rest/RestManager.js
+++ src/rest/RestManager.js
@@ -8,27 +8,40 @@ module.exports = class RestManager {
     this.client = client;
     this.userAgent = `Hitomi (https://github.com/IsisDiscord/hitomi, ${require("../../package.json").version})`;
 
-    this.apiURL = `${Constants.REST.BASE_URL}${this.client.options.rest.version}`;
+    this.apiURL = `${Constants.REST.BASE_URL}/v9`;
 
     this.api = buildRoute(this);
   };
 
   /**
    * Make an HTTP request to the Discord API
-   * @param {String} [method] The HTTP method to use
-   * @param {String} [url] URL to make the request to
-   * @param {Object} [data] The data to be sent
-   * @param {Boolean} [authenticate] Whether to authenticate the request
+   * @param {String} method The HTTP method to use
+   * @param {String} url URL to make the request to
+   * @param {Object} options The options to use on the request
+   * @param {Object} [options.data] The data to be sent
+   * @param {Boolean} [options.authenticate] Whether to authenticate the request
    */
-  async request(method, url, data, authenticate = true) {
+  async request(method, url, options) {
     const headers = {
       "User-Agent": this.userAgent,
       "Content-Type": "application/json"
     };
 
-    if (authenticate) headers.Authorization = `Bot ${this.client.token}`;
+    if (options.authenticate) headers.Authorization = `Bot ${this.client.token}`;
 
-    //return axios[method.toLowerCase()](`${this.apiURL}/${url.replace(/[/]?(\w+)/, '$1')}`, { headers, data });
+    return axios({
+      url: `${this.apiURL}/${url.replace(/[/]?(\w+)/, '$1')}`,
+      method: method.toLowerCase(),
+      data: options.data,
+      headers
+    }).then((data) => console.log(data.headers));
+
+    /*
+    Dessa forma não está funcionando, não sei o por quê
+    return axios[method.toLowerCase()](`${this.apiURL}/${url.replace(/[/]?(\w+)/, '$1')}`, {
+      data: options.data,
+      headers
+    }).then(console.log);*/
   }
 };
 
