ce7cbfd5b27b82d680ee5969f5af1b3f125043ea
Loliticos <familiadojorge@outlook.com>
2021-08-15 01:02:27 -0300

    feat() create message for test

--- test.js
+++ test.js
@@ -7,11 +7,12 @@ const client = new Client(process.env.DISCORD_TOKEN, {
   rest: { fetchAllUsers: true }
 });
 
-client.start()
-  .then(() => {
-    /*
-    Enviar mensgagem:
+client.on("messageCreate", (data) => {
+  if (data.content === "log") {
     client.rest.api
       .channels["857279585568686100"]
-      .messages.post({ data: { content: "oi" }, authenticate: true })*/
-  })
+      .messages.post({ data: { content: "oi" }, authenticate: true })
+  }
+})
+
+client.start();
