3ad9ab34c5d220197850e2b6c6550d7879cc0b27
Loliticos <familiadojorge@outlook.com>
2021-08-16 00:45:00 -0300

    feat() document changes and create jsdoc

---
+++ src/structures/Base.js
@@ -0,0 +1,25 @@
+/**
+ * Represents a discord structure.
+ */
+class Base {
+  constructor(client) {
+    /**
+     * The client that instantiated this
+     * @name Base#client
+     * @type {Client}
+     * @readonly
+     */
+    Object.defineProperty(this, "client", { value: client });
+  }
+
+
+  _update(data) {;
+    return data;
+  }
+
+  valueOf() {
+    return this.id;
+  }
+}
+
+module.exports = Base;
--- src/structures/Message.js
+++ src/structures/Message.js
@@ -1,38 +1,140 @@
-const User = require("./User");
+const Base = require("./Base");
 
-module.exports = class Message {
+/**
+  * Represents a discord message
+  * @extends {Base}
+  */
+class Message extends Base {
+  /**
+   * @param {Client} client Hitomi Client
+   * @param {Object} data The message structure data
+   */
   constructor (data, client) {
-    Object.defineProperty(this, "client", { value: client });
+    super(client);
 
+    /**
+     * Mentions included in the message
+     * @typedef {Object} MessageMentions
+     * @property {Boolean} everyone Whether the message mentions everyone
+     * @property {Array<String>} users The users that were mentioned in the message
+     * @property {Array<String>} roles The roles that were mentioned in the message
+     * @property {Array<String>} channels The channels that were mentioned in the message
+     */
+    this.mentions = { everyone: null, users: null, roles: null, channels: null };
+
+    this._update(data);
+  }
+
+  _update (data) {
+    /**
+     * Message ID
+     * @type {String}
+     */
     this.id = data.id;
-    this.type = data.type;
-    this.content = data.content || "";
-    this.channel = data.channel_id;//this.client.channels.get(data.channel_id);
-    this.author = new User(data.author);
-    this.attachments = data.attachments;
-    this.embeds = data.embeds;
-    this.components = data.components;
-    this.timestamp = new Date(data.timestamp);
-    this.flags = data.flags;
-    this.mentions = {
-      everyone: data.mention_everyone,
-      users: data.mentions,
-      roles: data.mention_roles,
-      channels: data.mention_channels
-    };
+
+    if (data.channel_id) {
+      /**
+       * The channel id in which the message was sent
+       * @type {String}
+       */
+      this.channelID = data.channel_id;
+    }
+
+    if (data.guild_id) {
+      /**
+       * The guild id in which the message was sent
+       * @type {String}
+       */
+      this.guildID = data.guild_id;
+    }
+
+    if (data.user?.id) {
+      /**
+       * The user that created the message
+       * @type {User}
+       */
+      this.author = this.client.users.get(data.user.id);
+    }
+
+    if (data.content) {
+      /**
+       * The message content
+       * @type {String}
+       */
+      this.content = data.content;
+    }
+
+    if (data.timestamp) {
+      /**
+       * The time the message was created
+       * @type {Data}
+       */
+      this.timestamp = new Date(data.timestamp);
+    }
+
+    if (data.mentions) {
+      this.mentions = {
+        everyone: Boolean(data.mention_everyone),
+        users: data.mentions,
+        roles: data.mention_roles,
+        channels: data.mention_channels
+      }
+    }
+
+    if (data.attachments) {
+      /**
+       * The attachments from the message
+       * @type {Array}
+       */
+      this.attachments = data.attachments;
+    }
+
+    if (data.embeds?.length) {
+      /**
+       * The embeds from the message
+       * @type {Array}
+       */
+      this.embeds = data.embeds;
+    }
+
+    if (data.type) {
+      /**
+       * The type of the message
+       * @type {Number}
+       */
+      this.type = data.type;
+    }
+
+    if (data.flags) {
+      /**
+       * The message flags
+       * @type {Number}
+       */
+      this.flags = data.flags ?? 0;
+    }
+
+    if (data.components?.length) {
+      /**
+       * The message components
+       * @type {Array}
+       */
+      this.components = data.components;
+    }
   }
 
   delete () {
-    return this.client.rest.api.channels[this.channel].messages[this.id].delete();
+    return this.client.rest.api.channels[this.channelID].messages[this.id].delete();
   }
 
   edit (options) {
     if (typeof (options) === "string") options = { content: options };
 
-    return this.client.rest.api.channels[this.channel].messages[this.id].patch({ data: options });
+    return this.client.rest.api.channels[this.channelID].messages[this.id].patch({ data: options });
   }
 
   addReaction (reaction) {
-    return this.client.addMessageReaction(this.id, this.channel, reaction)
+    return this.client.addMessageReaction(this.id, this.channelID, reaction)
   }
 };
+
+module.exports = Message;
