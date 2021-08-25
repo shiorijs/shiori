ae82cc2209e01a75cdbf8593678e1c7ce6656f4a
Loliticos <familiadojorge@outlook.com>
2021-08-25 11:16:50 -0300

    style() extra whitespace

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -119,10 +119,12 @@ module.exports = {
 
 function createEnum (keys) {
   const obj = {};
+
   for (const [index, key] of keys.entries()) {
     if (key === null) continue;
     obj[key] = index;
     obj[index] = key;
   }
+  
   return obj;
 }
