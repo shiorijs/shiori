5bbf54cc4236952219d5734cbb3e98bb74977057
Loliticos <loliticos@outlook.com>
2021-09-03 16:06:59 -0300

    fixes()

--- src/client/ClientUtils.js
+++ src/client/ClientUtils.js
@@ -1,7 +1,3 @@
-const Constants = require("../utils/Constants");
-const Guild = require("../structures/Guild");
-const User = require("../structures/User");
-
 class ClientUtils {
   #client;
 
@@ -26,58 +22,15 @@ class ClientUtils {
   }
 
   /**
-    * Returns the image of a target.
-    * If target is a guild you can use `image(Guild).banner()` or `image(Guild).icon()`
-    * If target is a user you can use `image(User).avatar()` or `image(User).default()`
-    * @param {User | Guild} target The target to return the image of
-    * @returns {Function} The image.
-    */
-  image (target) {
-    const defaultFormat = this.#client.options.defaultFormat;
-    const defaultSize = this.#client.options.defaultSize;
-
-    if (target instanceof Guild) {
-      return {
-        icon (format = defaultFormat, size = defaultSize, dynamic = false) {
-          return makeImageUrl(`icons/${target.id}/${target.iconHash}`, format, size, dynamic);
-        },
-        banner (format = defaultFormat, size = defaultSize, dynamic = false) {
-          return makeImageUrl(`banners/${target.id}/${target.iconHash}`, format, size, dynamic);
-        }
-      };
-    }
-
-    if (target instanceof User) {
-      return {
-        default (size = defaultSize) {
-          return makeImageUrl(`embed/avatars/${target.discriminator % 5}`, "png", size, false);
-        },
-        avatar (format = defaultFormat, size = defaultSize, dynamic = false) {
-          return makeImageUrl(`avatars/${target.id}/${target.avatarHash}`, format, size, dynamic);
-        }
-      };
-    }
-
-    throw new Error("Invalid target");
-  }
-
-  /**
    * setTimeout but as a promise.
-   * @param {number} ms Timeout in MS
-   * @returns {Promise<boolean>}
+   * @params {Number} ms Timeout in MS
+   * @returns {Promise<Boolean>}
    */
-   static delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function makeImageUrl (root, format, size, dynamic) {
-  const { ImageFormats, ImageSizes } = Constants;
-
-  if (format && !ImageFormats.includes(format)) throw new Error(`Invalid image format: ${format}`);
-  if (size && !ImageSizes.includes(size)) throw new RangeError(`Invalid image size: ${size}`);
-
-  if (dynamic) format = root.split("/").slice(-1)[0].startsWith("a_") ? "gif" : format;
-
-  return `https://cdn.discordapp.com/${root}.${format ?? "webp"}${size ? `?size=${size}` : ""}`;
+  static async delay (ms) {
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(true), ms);
+    });
+  }
 }
 
 module.exports = ClientUtils;
