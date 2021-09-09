c054ce607bc60f2ce1c892be541dc4a8a425c1d1
Loliticos <loliticos@outlook.com>
2021-09-09 19:56:22 -0300

    fix: remove ChannelTypes enum

--- types/index.d.ts
+++ types/index.d.ts
@@ -19,20 +19,6 @@ export type GuildFeatures =
 
 // Enums
 
-export enum ChannelTypes {
-  GUILD_TEXT = 0,
-  DM = 1,
-  GUILD_VOICE = 2,
-  GROUP_DM = 3,
-  GUILD_CATEGORY = 4,
-  GUILD_NEWS = 5,
-  GUILD_STORE = 6,
-  GUILD_NEWS_THREAD = 10,
-  GUILD_PUBLIC_THREAD = 11,
-  GUILD_PRIVATE_THREAD = 12,
-  GUILD_STAGE_VOICE = 13
-};
-
 export enum MessageType {
   DEFAULT	= 0,
   RECIPIENT_ADD	= 1,
@@ -519,7 +505,7 @@ export class BaseGuildChannel extends Channel {
   public parentId: Snowflake;
   public nsfw: boolean;
   public guildId: Snowflake;
-  public type: ChannelTypes;
+  public type: Constants["ChannelTypes"][keyof Constants["ChannelTypes"]];;
   public messages: Collection<Snowflake, Message>;
 }
 
