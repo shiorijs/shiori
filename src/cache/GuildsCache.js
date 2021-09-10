7b3f059b2b64d427afaf49495920e21f849e2bc2
Loliticos <loliticos@outlook.com>
2021-09-10 12:29:11 -0300

    feat: jsodc for Cache

--- src/cache/GuildsCache.js
+++ src/cache/GuildsCache.js
@@ -2,16 +2,28 @@ const Guild = require("../structures/Guild");
 const Endpoints = require("../rest/Endpoints");
 const Cache = require("./Cache");
 
+/**
+ * Represents the Guilds Cache Manager.
+ * @extends {Cache}
+ */
 class GuildsCache extends Cache {
   #client;
 
+  /**
+   * @param {Client} client The instantiating client
+   */
   constructor (client) {
     super(client.options.cache.guilds, Guild);
     this.#client = client;
   }
 
-  async fetch (userId) {
-    return await this.#client.rest.request("get", Endpoints.USER(userId));
+  /**
+    * Makes a HTTP request in order to fetch the guild
+    * @param {string} guildId The guild id to fetch
+    * @returns {Guild}
+    */
+  async fetch (guildId) {
+    return await this.#client.rest.request("get", Endpoints.USER(guildId));
   }
 }
 
